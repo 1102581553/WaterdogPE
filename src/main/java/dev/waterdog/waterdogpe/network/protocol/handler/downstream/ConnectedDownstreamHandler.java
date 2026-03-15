6868611d839326b0c28089ccb598a13ab462597f
1102581553 <pheyeji@gmail.com>
2026-03-15 18:00:10 +0000

    Update ConnectedDownstreamHandler.java

--- src/main/java/dev/waterdog/waterdogpe/network/protocol/handler/downstream/ConnectedDownstreamHandler.java
+++ src/main/java/dev/waterdog/waterdogpe/network/protocol/handler/downstream/ConnectedDownstreamHandler.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2022 WaterdogTEAM
+ *
  * Licensed under the GNU General Public License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -26,9 +27,16 @@ import dev.waterdog.waterdogpe.network.serverinfo.ServerInfo;
 import dev.waterdog.waterdogpe.player.ProxiedPlayer;
 import dev.waterdog.waterdogpe.utils.types.TranslationContainer;
 import org.cloudburstmc.protocol.bedrock.PacketDirection;
+import org.cloudburstmc.protocol.bedrock.data.entity.EntityLinkData;
+import org.cloudburstmc.protocol.bedrock.packet.AddEntityPacket;
+import org.cloudburstmc.protocol.bedrock.packet.AddPlayerPacket;
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket;
+import org.cloudburstmc.protocol.bedrock.packet.BossEventPacket;
 import org.cloudburstmc.protocol.bedrock.packet.DisconnectPacket;
+import org.cloudburstmc.protocol.bedrock.packet.PlayerListPacket;
 import org.cloudburstmc.protocol.bedrock.packet.PlayStatusPacket;
+import org.cloudburstmc.protocol.bedrock.packet.RemoveEntityPacket;
+import org.cloudburstmc.protocol.bedrock.packet.SetEntityLinkPacket;
 import org.cloudburstmc.protocol.bedrock.packet.SetLocalPlayerAsInitializedPacket;
 import org.cloudburstmc.protocol.bedrock.packet.TransferPacket;
 import org.cloudburstmc.protocol.common.PacketSignal;
@@ -49,12 +57,68 @@ public class ConnectedDownstreamHandler extends AbstractDownstreamHandler {
         }
 
         PacketSignal signal = super.handlePacket(packet);
+
         if (!player.getPluginPacketHandlers().isEmpty()) {
             for (PluginPacketHandler handler : this.player.getPluginPacketHandlers()) {
                 signal = mergeSignals(signal, handler.handlePacket(packet, PacketDirection.CLIENT_BOUND));
             }
         }
+
+        if (signal != Signals.CANCEL) {
+            this.updateClientEntityTracker(packet);
+        }
+
         return signal;
+    }
+
+    private void updateClientEntityTracker(BedrockPacket packet) {
+        if (packet instanceof AddPlayerPacket addPlayerPacket) {
+            this.player.trackClientEntity(addPlayerPacket.getUniqueEntityId());
+            return;
+        }
+
+        if (packet instanceof AddEntityPacket addEntityPacket) {
+            this.player.trackClientEntity(addEntityPacket.getUniqueEntityId());
+            return;
+        }
+
+        if (packet instanceof RemoveEntityPacket removeEntityPacket) {
+            this.player.untrackClientEntity(removeEntityPacket.getUniqueEntityId());
+            return;
+        }
+
+        if (packet instanceof SetEntityLinkPacket linkPacket) {
+            EntityLinkData link = linkPacket.getEntityLink();
+            if (link != null) {
+                if (link.getType() == EntityLinkData.Type.REMOVE) {
+                    this.player.getEntityLinks().remove(link.getFrom());
+                } else {
+                    this.player.getEntityLinks().put(link.getFrom(), link.getTo());
+                }
+            }
+            return;
+        }
+
+        if (packet instanceof BossEventPacket bossEventPacket) {
+            if (bossEventPacket.getAction() == BossEventPacket.Action.REMOVE) {
+                this.player.getBossbars().remove(bossEventPacket.getBossUniqueEntityId());
+            } else {
+                this.player.getBossbars().add(bossEventPacket.getBossUniqueEntityId());
+            }
+            return;
+        }
+
+        if (packet instanceof PlayerListPacket playerListPacket) {
+            if (playerListPacket.getAction() == PlayerListPacket.Action.ADD) {
+                for (PlayerListPacket.Entry entry : playerListPacket.getEntries()) {
+                    this.player.getPlayers().add(entry.getUuid());
+                }
+            } else if (playerListPacket.getAction() == PlayerListPacket.Action.REMOVE) {
+                for (PlayerListPacket.Entry entry : playerListPacket.getEntries()) {
+                    this.player.getPlayers().remove(entry.getUuid());
+                }
+            }
+        }
     }
 
     @Override
@@ -64,8 +128,8 @@ public class ConnectedDownstreamHandler extends AbstractDownstreamHandler {
         }
 
         this.player.setAcceptPlayStatus(false);
-        RewriteData rewriteData = this.player.getRewriteData();
 
+        RewriteData rewriteData = this.player.getRewriteData();
         if (!rewriteData.hasImmobileFlag()) {
             injectEntityImmobile(this.player.getConnection(), rewriteData.getEntityId(), false);
         }
@@ -78,7 +142,6 @@ public class ConnectedDownstreamHandler extends AbstractDownstreamHandler {
         this.player.getProxy().getEventManager().callEvent(event);
 
         this.player.armTransferSettleWindow();
-
         this.player.getProxy().getScheduler().scheduleDelayed(() -> {
             if (this.player.isConnected()
                     && this.player.getConnection() != null
@@ -101,7 +164,12 @@ public class ConnectedDownstreamHandler extends AbstractDownstreamHandler {
             serverInfo = this.player.getProxy().getServerInfo(packet.getAddress(), packet.getPort());
         }
 
-        FastTransferRequestEvent event = new FastTransferRequestEvent(serverInfo, this.player, packet.getAddress(), packet.getPort());
+        FastTransferRequestEvent event = new FastTransferRequestEvent(
+                serverInfo,
+                this.player,
+                packet.getAddress(),
+                packet.getPort()
+        );
         this.player.getProxy().getEventManager().callEvent(event);
 
         if (!event.isCancelled() && event.getServerInfo() != null) {
@@ -112,14 +180,20 @@ public class ConnectedDownstreamHandler extends AbstractDownstreamHandler {
             }
             return Signals.CANCEL;
         }
+
         return PacketSignal.UNHANDLED;
     }
 
     @Override
     public final PacketSignal handle(DisconnectPacket packet) {
-        if (this.player.sendToFallback(this.connection.getServerInfo(), ReconnectReason.SERVER_KICK, packet.getKickMessage())) {
+        if (this.player.sendToFallback(
+                this.connection.getServerInfo(),
+                ReconnectReason.SERVER_KICK,
+                packet.getKickMessage()
+        )) {
             return Signals.CANCEL;
         }
+
         this.player.disconnect(new TranslationContainer("waterdog.downstream.kicked", packet.getKickMessage()));
         return Signals.CANCEL;
     }
